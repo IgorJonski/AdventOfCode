8c3e1b3b1f4cb57f864d4d3088680ec4758670f4
IgorJonski <jonskiigor@gmail.com>
2024-01-26 01:02:45 +0100

    Day 15 (part 2)

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,7 +1,8 @@
 import java.io.File
 
 fun main() {
-    day15task1()
+//    day15task1()
+    day15task2()
 }
 
 fun day15task1() {
@@ -29,3 +30,46 @@ fun hash(message: String): Long {
     }
     return hashValue
 }
+
+fun day15task2() {
+    val file = File("src/main/resources/day15_input.txt")
+    if (!file.exists()) {
+        println("File not found")
+        return
+    }
+
+    val initializationSequence = file.readText()
+    val sequences = initializationSequence.split(",").toList()
+    val boxes = MutableList<MutableList<LensSlot>>(256) { mutableListOf() }
+    for (sequence in sequences) {
+        if (sequence.contains("=")) {
+            val label = sequence.substringBefore("=")
+            val focalLength = sequence.substringAfter("=").toLong()
+            val hash = hash(label).toInt()
+            val existingLensSlot = boxes[hash].firstOrNull { it.label == label }
+            if (existingLensSlot != null) {
+                existingLensSlot.focalLength = focalLength
+            } else {
+                boxes[hash].add(LensSlot(label, focalLength))
+            }
+        } else {
+            val label = sequence.substringBefore("-")
+            val hash = hash(label).toInt()
+            boxes[hash].removeIf { it.label == label }
+        }
+    }
+
+    var sum = 0L
+    for (boxIndex in boxes.indices) {
+        for (lensSlotIndex in boxes[boxIndex].indices) {
+            val onePlusBoxIndex = boxIndex + 1
+            val slotNumber = lensSlotIndex + 1
+            val focalLength = boxes[boxIndex][lensSlotIndex].focalLength
+            sum += onePlusBoxIndex * slotNumber * focalLength
+        }
+    }
+
+    println(sum)
+}
+
+data class LensSlot(val label: String, var focalLength: Long)
